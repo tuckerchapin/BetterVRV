38f6ab24e292cbe3801fd91b648e2702032f4ea5
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-12-01 00:58:04 -0500

    Basic seeking. No global settings yet.

--- injections/player.js
+++ injections/player.js
@@ -1,2 +1,58 @@
 let vrvPlayer = document.querySelector("video#player_html5_api");
-console.log(vrvPlayer);
+
+let shortcuts = {
+    majorSeekForward: "KeyL",
+    majorSeekBackward: "KeyJ",
+    minorSeekForward: "ArrowRight",
+    minorSeekBackward: "ArrowLeft",
+    playPause: "KeyK",
+    pause: "KeyP",
+}
+
+let settings = {
+    majorSeekIncrement: 10,
+    minorSeekIncrement: 5,
+}
+
+let debugLog = false;
+
+document.onkeydown = (e) => {
+    switch (e.code) {
+        case shortcuts.majorSeekForward:
+            if (debugLog) console.log(`Skipped forward ${settings.majorSeekIncrement}s.`);
+            vrvPlayer.currentTime = vrvPlayer.currentTime + settings.majorSeekIncrement;
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case shortcuts.majorSeekBackward:
+            if (debugLog) console.log(`Skipped backward ${settings.majorSeekIncrement}s.`);
+            vrvPlayer.currentTime = vrvPlayer.currentTime - settings.majorSeekIncrement;
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case shortcuts.minorSeekForward:
+            if (debugLog) console.log(`Skipped forward ${settings.minorSeekIncrement}s.`);
+            vrvPlayer.currentTime = vrvPlayer.currentTime + settings.minorSeekIncrement;
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case shortcuts.minorSeekBackward:
+            if (debugLog) console.log(`Skipped backward ${settings.minorSeekIncrement}s.`);
+            vrvPlayer.currentTime = vrvPlayer.currentTime - settings.minorSeekIncrement;
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case shortcuts.playPause:
+            if (debugLog) console.log(`Play/pause toggle used to ${vrvPlayer.paused ? "play" : "pause"}.`)
+            vrvPlayer.paused ? vrvPlayer.play() : vrvPlayer.pause();
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+        case shortcuts.pause:
+            if (debugLog) console.log("Paused.");
+            vrvPlayer.pause();
+            e.stopPropagation();
+            e.preventDefault();
+            break;
+    }
+};
