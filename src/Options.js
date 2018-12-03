a2b27993da34195492fe022991221ed6b6b2379f
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-12-03 02:43:49 -0500

    Uhhh.

--- src/Options.js
+++ src/Options.js
@@ -9,26 +9,28 @@ import './styles/Options.css';
 
 const INDEV = true;
 
+const DEFAULT_OPTIONS = {
+    "hideDescriptions": true,
+    "hideThumbnails": true,
+    "showWatchedThumbnails": false,
+
+    "majorSeekIncrement": 10,
+    "minorSeekIncrement": 5,
+
+    "majorSeekForward": ["16+76", ""],
+    "majorSeekBackward": ["16+74", ""],
+    "minorSeekForward": ["39", "76"],
+    "minorSeekBackward": ["37", "74"],
+    "playPause": ["75", "32"],
+    "pause": ["80", ""],
+    "toggleFullscreen": ["70", ""],
+}
+
 class Options extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            "hideDescriptions": true,
-            "hideThumbnails": true,
-            "showWatchedThumbnails": false,
-
-            "majorSeekIncrement": 10,
-            "minorSeekIncrement": 5,
-
-            "majorSeekForward": ["16+76", ""],
-            "majorSeekBackward": ["16+74", ""],
-            "minorSeekForward": ["39", "76"],
-            "minorSeekBackward": ["37", "74"],
-            "playPause": ["75", "32"],
-            "pause": ["80", ""],
-            "toggleFullscreen": ["70", ""],
-        };
+        this.state = DEFAULT_OPTIONS;
 
         this.load();
     }
@@ -43,8 +45,9 @@ class Options extends Component {
     }
 
     save(newState) {
+        if (INDEV) console.log(newState);
         this.setState(
-            {minorSeekIncrement: newState},
+            newState,
             () => {
                 if (!INDEV) {
                     chrome.storage.sync.set(
@@ -54,6 +57,22 @@ class Options extends Component {
                 }
             }
         )
+    }
+
+    resetSettings() {
+        if (window.confirm('Are you sure you want to return all settings to their defaults?')) {
+            this.setState(
+                DEFAULT_OPTIONS,
+                () => {
+                    if (!INDEV) {
+                        chrome.storage.sync.set(
+                            DEFAULT_OPTIONS,
+                            () => {if (chrome.runtime.lastError) console.error(chrome.runtime.lastError);}
+                        );
+                    }
+                }
+            )
+        }
     }
 
     render() {
@@ -73,11 +92,7 @@ class Options extends Component {
                                 description="All thumbnails will be blurred. The placeholder image used by the player while loading the episode will also be blurred."
                                 controlType="toggle"
                                 value={this.state.hideThumbnails}
-                                onChange={
-                                    (newValue) => this.setState(
-                                        {hideThumbnails: newValue},
-                                        () => {if (!INDEV) this.save();})
-                                }
+                                onChange={(newValue) => this.save({hideThumbnails: newValue})}
                             />
                             <ControlRow title="Show Watched Thumbnails"
                                 description="Thumbnails on episodes you've already watched will not be blurred."
@@ -85,23 +100,13 @@ class Options extends Component {
                                 value={this.state.showWatchedThumbnails}
                                 disabled={!this.state.hideThumbnails}
                                 subrow={true}
-                                onChange={
-                                    (newValue) => this.setState(
-                                        {showWatchedThumbnails: newValue},
-                                        () => {if (!INDEV) this.save();}
-                                    )
-                                }
+                                onChange={(newValue) => this.save({showWatchedThumbnails: newValue})}
                             />
                             <ControlRow title="Hide Descriptions"
                                 description="All episode descriptions will be hidden."
                                 controlType="toggle"
                                 value={this.state.hideDescriptions}
-                                onChange={
-                                    (newValue) => this.setState(
-                                        {hideDescriptions: newValue},
-                                        () => {if (!INDEV) this.save();}
-                                    )
-                                }
+                                onChange={(newValue) => this.save({hideDescriptions: newValue})}
                             />
                     </ControlPanel>
 
@@ -110,23 +115,13 @@ class Options extends Component {
                             description="How far forward/backward the major seek shortcuts will skip."
                             controlType="number"
                             value={this.state.majorSeekIncrement}
-                            onChange={
-                                (newValue) => this.setState(
-                                    {majorSeekIncrement: newValue},
-                                    () => {if (!INDEV) this.save();}
-                                )
-                            }
+                            onChange={(newValue) => this.save({majorSeekIncrement: newValue})}
                         />
                         <ControlRow title="Minor Seek Increment"
                             description="How far forward/backward the minor seek shortcuts will skip."
                             controlType="number"
                             value={this.state.minorSeekIncrement}
-                            onChange={
-                                (newValue) => this.setState(
-                                    {minorSeekIncrement: newValue},
-                                    () => {if (!INDEV) this.save();}
-                                )
-                            }
+                            onChange={(newValue) => this.save({minorSeekIncrement: newValue})}
                         />
                     </ControlPanel>
 
@@ -134,39 +129,39 @@ class Options extends Component {
                         <KeyBindRow
                             title="Toggle Fullscreen"
                             value={this.state.toggleFullscreen}
-                            onChange={console.log}
+                            onChange={(newValue) => this.save({toggleFullscreen: newValue})}
                         />
                         <KeyBindRow
                             title="Toggle Play/Pause"
                             value={this.state.playPause}
-                            onChange={console.log}
+                            onChange={(newValue) => this.save({playPause: newValue})}
                         />
                         <KeyBindRow
                             title="Seek Forward - Major"
                             value={this.state.majorSeekForward}
-                            onChange={console.log}
+                            onChange={(newValue) => this.save({majorSeekForward: newValue})}
                         />
                         <KeyBindRow
                             title="Seek Forward - Minor"
                             value={this.state.minorSeekForward}
                             subrow={true}
-                            onChange={console.log}
+                            onChange={(newValue) => this.save({minorSeekForward: newValue})}
                         />
                         <KeyBindRow
                             title="Seek Backward - Major"
                             value={this.state.majorSeekBackward}
-                            onChange={console.log}
+                            onChange={(newValue) => this.save({majorSeekBackward: newValue})}
                         />
                         <KeyBindRow
                             title="Seek Backward - Minor"
                             value={this.state.minorSeekBackward}
                             subrow={true}
-                            onChange={console.log}
+                            onChange={(newValue) => this.save({minorSeekBackward: newValue})}
                         />
                     </ControlPanel>
 
                     <div id="reset-container">
-                        <div id="reset-button">
+                        <div id="reset-button" onClick={() => this.resetSettings()}>
                             reset all settings to defaults
                         </div>
                     </div>
