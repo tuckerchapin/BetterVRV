298a850071c6baf5188405b05fb6632d0f56726b
Tucker Chapin <tuckerchapin@tuckerbook-air.local>
2018-12-06 01:36:51 -0500

    Fixed manifest listing. and insertCSS loads.

--- public/content_scripts/top-site/js/vrvStyling.js
+++ public/content_scripts/top-site/js/vrvStyling.js
@@ -2,15 +2,15 @@ chrome.storage.sync.get(
     DEFAULT_OPTIONS,
     (options) => {
         if (options.hideDescriptions) {
-            insertCSS("content_scripts/css/hideDescriptions.css");
+            insertCSS("content_scripts/top-site/css/hideDescriptions.css");
         }
 
         if (options.hideThumbnails) {
-            insertCSS("content_scripts/css/hideThumbnails.css");
+            insertCSS("content_scripts/top-site/css/hideThumbnails.css");
         }
 
         if (options.showWatchedThumbnails) {
-            insertCSS("content_scripts/css/showWatchedThumbnails.css");
+            insertCSS("content_scripts/top-site/css/showWatchedThumbnails.css");
         }
 
         addSettingsDropdown();
